adb14fbd1526bfb2c3b23ef12ea79a748887cadd
Lukas Moos <lukas.stephan.moos@muk.thm.de>
2024-06-01 18:10:51 +0200

    Added Login and started Tailwind integration.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { useState } from "react";
+import Login from "./Components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p> Currently in Debug Page.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [authenticated, changeAuthentication] = useState("false");
+
+  const authentication = () => {
+    changeAuthentication(!authenticated);
+  };
+
+  return authenticated ? (
+    <Login onAuthentication={authentication} />
+  ) : (
+    LoggedInView()
   );
-}
+};
+
+const LoggedInView = () => {
+  return <div>Logged In</div>;
+};
 
 export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,5 @@
+const Button = ({ text, color, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+export default Button;
