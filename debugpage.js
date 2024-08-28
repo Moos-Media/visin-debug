bd6907ca4dc42a44ff0c170fd7e7ab795e51c6c2
Lukas Moos <lukas.stephan.moos@muk.thm.de>
2024-08-28 10:44:27 +0200

    Added Login on enter keypress.

--- debugpage.js
+++ debugpage.js
@@ -81,6 +81,7 @@ const createLoginUI = () => {
   passwordInput = createInput("", "password");
   loginButton = createButton("Login");
 
+  passwordInput.changed(login);
   loginButton.mousePressed(login);
 };
 
